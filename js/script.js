df5408b9477bd41bcc5e1440f875dfd28467dbf7
German Bulas <b.german2014@mail.ru>
2024-02-02 20:10:14 +0200

    Co-authored-by: Galacticblack <Galacticblack@users.noreply.github.com>

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+
+let vasi = 'German'
+let myAge = 9
+
+ 
+
+console.log(myName);
+console.log(myAge);
+
+
+
+
+// console.log("привет мир как дела")
+// console.log("хорошо!!!")
+// console.log("у нас тоже")
+// console.log("это здорово!!!!!!")
--- script.js
+++
@@ -1,4 +0,0 @@
-console.log("привет мир как дела")
-console.log("хорошо!!!")
-console.log("у нас тоже")
-console.log("это здорово!!!!!!")
